b70b024143433a2abe1f8215885424f365a78ebc
Yuya Hashirizaki <hashirizaki61@gmail.com>
2021-04-24 23:06:30 -0700

    tweak for images

--- React_WebSite/src/components/InfoSection/Data.js
+++ React_WebSite/src/components/InfoSection/Data.js
@@ -1,53 +1,53 @@
 export const home0bjOne = {
-  id: 'about',
+  id: "about",
   lightBg: false,
   lightText: true,
   loightTextDesc: true,
-  topLine: 'dummy Text',
-  headline: 'dummy Text dummy Text dummy Text dummy Text',
+  topLine: "dummy Text",
+  headline: "dummy Text dummy Text dummy Text dummy Text",
   description:
-    'dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text',
-  buttonLabel: 'dummy Text',
+    "dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text",
+  buttonLabel: "dummy Text",
   imgStart: false,
-  img: require('../../images/svg-1.svg'),
-  alt: '1',
+  img: require("../../images/image.svg"),
+  alt: "1",
   dark: true,
   primary: true,
-  darkText: false
+  darkText: false,
 };
 
 export const home0bjTwo = {
-  id: 'discover',
+  id: "discover",
   lightBg: true,
   lightText: false,
   loightTextDesc: false,
-  topLine: 'dummy Text',
-  headline: 'dummy Text dummy Text dummy Text dummy Text',
+  topLine: "dummy Text",
+  headline: "dummy Text dummy Text dummy Text dummy Text",
   description:
-    'dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text',
-  buttonLabel: 'dummy Text',
+    "dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text",
+  buttonLabel: "dummy Text",
   imgStart: true,
-  img: require('../../images/svg-1.svg'),
-  alt: '1',
+  img: require("../../images/image.svg"),
+  alt: "2",
   dark: false,
   primary: false,
-  darkText: true
+  darkText: true,
 };
 
 export const home0bjThree = {
-  id: 'signup',
+  id: "signup",
   lightBg: true,
   lightText: false,
   loightTextDesc: false,
-  topLine: 'dummy Text',
-  headline: 'dummy Text dummy Text dummy Text dummy Text',
+  topLine: "dummy Text",
+  headline: "dummy Text dummy Text dummy Text dummy Text",
   description:
-    'dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text',
-  buttonLabel: 'dummy Text',
+    "dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text dummy Text",
+  buttonLabel: "dummy Text",
   imgStart: false,
-  img: require('../../images/svg-1.svg'),
-  alt: '1',
+  img: require("../../images/image.svg"),
+  alt: "3",
   dark: false,
   primary: false,
-  darkText: true
+  darkText: true,
 };
--- React_WebSite/src/components/Services/index.js
+++ React_WebSite/src/components/Services/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Icon1 from '../../images/svg-2.svg';
-import Icon2 from '../../images/svg-2.svg';
-import Icon3 from '../../images/svg-2.svg';
+import Icon1 from '../../images/image.svg';
+import Icon2 from '../../images/image.svg';
+import Icon3 from '../../images/image.svg';
 import {
   ServicesContainer,
   ServicesH1,
